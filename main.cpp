2bb27f0d95acc7f3994ad4aa24a21f5f98554589
RovishKataria <rohankataria2000@gmail.com>
2025-01-24 12:30:16 +0530

    main.cpp added

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#incldue<iostream>
+using namespace std;
+
+void bubbleSort() {
+    
+}
+
+void quickSort(){
+    
+}
+
+void mergeSort() {
+    
+}
+
+void insertionSort() {
+    
+}
+
+int main() {
+    cout<<"Hello World";
+    return 0;
+}
